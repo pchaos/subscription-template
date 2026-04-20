b4cc9fe508700ea9d896eb96dd9e3a84ce3953d0
pchaos <drifthua@gmail.com>
2026-04-20 14:51:08 +0800

    split gkd rules: 芭芭农场&兔兔礼包

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -7,11 +7,8 @@ export default defineGkdApp({
     {
       key: 100,
       name: '芭芭农场-领取肥料礼包',
-      desc: '在芭芭农场做任务/施肥页面点击[去领取]按钮领取肥料礼包。每天7点/12点/20点/22点可领取',
-      activityIds: [
-        'com.taobao.themis.container.app.TMSActivity',
-        'com.taobao.farm.activity.FarmActivity',
-      ],
+      desc: '在芭芭农场做任务/施肥页面点击领取按钮。每天7点/12点/20点/22点可领取',
+      activityIds: ['com.taobao.themis.container.app.TMSActivity'],
       rules: [
         {
           key: 0,
@@ -22,6 +19,24 @@ export default defineGkdApp({
           key: 1,
           name: '点击立即领取按钮',
           matches: '[text="立即领取"][clickable=true][visibleToUser=true]',
+        },
+       ],
+    },
+    {
+      key: 101,
+      name: '兔兔礼包',
+      desc: '在兔兔农场页面点击领取按钮',
+      activityIds: ['com.taobao.farm.activity.FarmActivity'],
+      rules: [
+        {
+          key: 0,
+          name: '点击领取按钮',
+          matches: '[text="领取"][clickable=true][visibleToUser=true]',
+        },
+        {
+          key: 1,
+          name: '点击去领取按钮',
+          matches: '[text="50.00"][clickable=true][visibleToUser=true]',
         },
       ],
     },
